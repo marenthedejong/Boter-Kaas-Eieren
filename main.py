88a43bf3078220ea41655f62873e1c419c78536c
Marenthe de Jong <57255299+marenthedejong@users.noreply.github.com>
2021-12-18 16:21:47 +0000

    Uitleg toegevoegd over de hyperparamters alfa en epsilon als een comment in de code.

--- main.py
+++ main.py
@@ -19,14 +19,17 @@ class MyAgent(MLAgent):
 random.seed(1)
 
 my_agent = MyAgent(alpha=0.9, epsilon=0.3)
+#Alfa is belangrijk om aan te kunnen geven hoe snel de agent oude kennis vervangt door nieuwe kennis. Het is dus de "leerfactor" van de Agent (zie fundament, 2.4). Hoe dichter het getal bij 1 zit, hoe sneller de nieuwe kennis overgenomen wordt.
+
+#Epsilon zorgt ervoor dat de Agent nieuwe zetten blijft proberen en niet steeds bij de oude zetten met de beste uitkomsten blijft. Hij doet steeds vaker een random zet als het getal dicht bij 1 zit. Het kan dus zijn dat deze random zet juist beter is dan de oude best bekende zet, de winning-rate wordt dan hoger. Als de random zet juist slechter is wordt de winning rate weer lager. 
 
 random_agent = RandomAgent()
 
 train_and_plot(
 	 agent=my_agent,
 	 validation_agent=random_agent,
-	 iterations=20,
-	 trainings=150,
+	 iterations=25,
+	 trainings=200,
 	 validations=1500)
 
 
@@ -38,13 +41,11 @@ my_agent = load('MyAgent_3000')
  
 my_agent.learning = True
 
-	 
 validation_agent = RandomAgent()
  
-validation_result = validate(agent_x=my_agent, agent_o=validation_agent, iterations=100)
- 
-plot_validation(validation_result)
+validation_result = validate(agent_x=my_agent, agent_o=validation_agent, iterations=200)
 
+plot_validation(validation_result)
  
 start(player_x=my_agent)
 
