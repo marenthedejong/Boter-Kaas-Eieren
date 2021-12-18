14ef67dbfc88249e24d9828840b2ee9bb48fae3c
Marenthe de Jong <57255299+marenthedejong@users.noreply.github.com>
2021-12-18 16:24:51 +0000

    Verdere uitleg over hyperparameters toegevoegd.

--- main.py
+++ main.py
@@ -19,6 +19,9 @@ class MyAgent(MLAgent):
 random.seed(1)
 
 my_agent = MyAgent(alpha=0.9, epsilon=0.3)
+
+#Hyperparameters. Hyperparameters zorgen ervoor dat je de Agent anders kan samenstellen. Als het ware pas je de samenstelling van een functie ermee aan. Er zijn twee hyperparameters in functie van MyAgent, alfa en epsilon:
+
 #Alfa is belangrijk om aan te kunnen geven hoe snel de agent oude kennis vervangt door nieuwe kennis. Het is dus de "leerfactor" van de Agent (zie fundament, 2.4). Hoe dichter het getal bij 1 zit, hoe sneller de nieuwe kennis overgenomen wordt.
 
 #Epsilon zorgt ervoor dat de Agent nieuwe zetten blijft proberen en niet steeds bij de oude zetten met de beste uitkomsten blijft. Hij doet steeds vaker een random zet als het getal dicht bij 1 zit. Het kan dus zijn dat deze random zet juist beter is dan de oude best bekende zet, de winning-rate wordt dan hoger. Als de random zet juist slechter is wordt de winning rate weer lager. 
